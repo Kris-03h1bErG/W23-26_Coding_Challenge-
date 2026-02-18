d1770f2acc096f028a4ec406c8b2164a6b5b1d2a
mm980899 <s980899@student.knoxschools.org>
2026-02-18 15:36:11 +0000

    basic prime

--- coding_challenge.py
+++ coding_challenge.py
@@ -1,3 +1,4 @@
+# copy this: python3 coding_challenge.py
 # Project Euler Problem #46
 # Goldbach Conjecture
 # Find the smallest odd composite that is not the sum of a prime and twice a square
@@ -6,7 +7,13 @@ squares = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 primes = []
 
 def prime_generator(start, stop=6000):
+    for num in range(stop):
+        for i in range(1, num):
+            print(i)
 
-def odd_composite_generator(start, primes, stop=6000):
+
+# def odd_composite_generator(start, primes, stop=6000):
+
+prime_generator(1)
 
 
