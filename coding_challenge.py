b6ae8729acb5858f44f55b05ad7df322cbdc13df
Kris-03h1bErG <s1007469@student.knoxschools.org>
2026-02-19 17:55:00 +0000

    Created Primes Generator

--- coding_challenge.py
+++ coding_challenge.py
@@ -6,14 +6,25 @@ odd_composites = []
 squares = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 primes = []
 
-def prime_generator(start, stop=6000):
-    for num in range(stop):
+# arguments: start: int , stop:  int 
+#starts at 2 normaly with no start value
+def prime_generator(start = 2, stop = 10):
+    primes
+    for num in range(start, stop):
+        counter = 0
+        #print(f"dividee number {num}")
         for i in range(1, num):
-            print(i)
+            if num % i == 0:
+                #print(f"passed num {num}")
+                counter += 1
+        if counter == 1:
+            #print(f"further passed num {num}")
+            primes.append(num)
+    return primes
 
 
 # def odd_composite_generator(start, primes, stop=6000):
 
-prime_generator(1)
+print(prime_generator(2,100))
 
 
