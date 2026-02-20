4bb673eb04accd279e153d65df8c9ed2081c6c8a
mm980899 <s980899@student.knoxschools.org>
2026-02-20 15:06:11 +0000

    first_test

--- coding_challenge.py
+++ coding_challenge.py
@@ -2,11 +2,11 @@
 # Project Euler Problem #46
 # Goldbach Conjecture
 # Find the smallest odd composite that is not the sum of a prime and twice a square
-odd_composites = []
+odd_composites_real = []
 squares = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 primes = []
 
-# takes a start and stop int > defult of 2 int > defult of 10
+# takes a start and stop int > default of 2 int > default of 10
 # returns list o' primes 
 def prime_generator(start = 2, stop = 10):
     primes = []
@@ -21,7 +21,7 @@ def prime_generator(start = 2, stop = 10):
             primes.append(num)
     return primes
 
-# takes a start and stop int > defult of 2 int > defult of 10
+# takes a start and stop int > default of 2 int > default of 10
 # returns list o' composites 
 def odd_composite_generator(start = 2, stop = 10):
     composites = []
@@ -36,11 +36,21 @@ def odd_composite_generator(start = 2, stop = 10):
     #print(composites)
     return composites
 
-def goldbachs_conjecture_checker(list_of_primes, list_of_odd_composites ,)
-    print("fuck")
+def goldbachs_conjecture_checker(start = 2, stop = 5000)
+    for composite in odd_composite_generator(start, stop):
+        for prime in prime_generator(start, stop):
+            for square in squares:
+                if composite == prime + 2 * square**2:
+                    continue
+                if composite != prime + 2 * square**2:
+                    return composite
 
-list_of_primes = prime_generator(2 , 5000)
-odd_composite_generator(2 , 5000)
+print(goldbachs_conjecture_checker)
+
+            
+
+
+
 
 
 
