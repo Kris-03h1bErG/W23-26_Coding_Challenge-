ab7a684f4af0f07977f6c18a0359dae83bd4ca46
mm980899 <s980899@student.knoxschools.org>
2026-03-06 15:31:48 +0000

    fixed counter with comments

--- coding_challenge.py
+++ coding_challenge.py
@@ -2,9 +2,6 @@
 # Project Euler Problem #46
 # Goldbach Conjecture
 # Find the smallest odd composite that is not the sum of a prime and twice a square
-odd_composites_real = []
-squares = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
-primes = []
 
 # takes a start and stop int > default of 2 int > default of 10
 # returns list o' primes 
@@ -21,6 +18,12 @@ def prime_generator(start = 2, stop = 10):
             primes.append(num)
     return primes
 
+def squares(start, stop):
+    s_list = []
+    for i in range(start, stop):
+        s_list.append(i)
+    return s_list
+
 # takes a start and stop int > default of 2 int > default of 10
 # returns list o' composites 
 def odd_composite_generator(start = 2, stop = 10):
@@ -36,24 +39,24 @@ def odd_composite_generator(start = 2, stop = 10):
     #print(composites)
     return composites
 
-def goldbachs_conjecture_checker(start = 5500, stop = 6000):
+def goldbachs_conjecture_checker(start = 5000, stop = 6000):
     for composite in odd_composite_generator(start, stop):
         counter = 0
         print(composite)
-        for prime in prime_generator(start, stop):
-            for square in squares:
+        for prime in prime_generator(1, stop):
+            for square in squares(1, 100):
                 if prime + 2 * square**2 == composite:
                     counter += 1
                     print(counter)
                     continue
-                if prime + 2 * square**2 != composite:
-                    continue
-                if counter == 0:
-                    return composite
+    if counter == 0:
+        return composite
 
 print(goldbachs_conjecture_checker())
-
             
+# If you could revise this to make it less brute-forcey, what would you do?
+
+# Add more logic to start and stop to avoid wasting time on equations that obviously won't solve the equation. (Like when the square is large enough to be bigger than the composite when squared)
 
 
 
